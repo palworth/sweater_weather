30d78654132b2a31dd05ee742b0ccd45bb68c743
Pierce Alworth <pbalworth@gmail.com>
2020-04-18 15:05:48 -0500

    Updte spec for forcast endpoint

--- spec/requests/api/v1/forcast_from_location_spec.rb
+++ spec/requests/api/v1/forcast_from_location_spec.rb
@@ -1,10 +1,12 @@
-# require 'rails_helper'
-#
-# describe "Forcast API" do
-#   it "sends weather data specific to denver" do
-#
-#     get '/api/v1/locations'
-#
-#     expect(response).to be_successful
-#   end
-# end
+require 'rails_helper'
+
+describe "Forcast API" do
+  it "sends weather data specific to denver" do
+
+    location = "denver,co"
+
+    get "/api/v1/forecast?location=#{location}"
+
+    expect(response).to be_successful
+  end
+end
