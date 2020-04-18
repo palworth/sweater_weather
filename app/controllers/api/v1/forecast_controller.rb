a9d49a47fd5ddd005841691475b50903189f28ee
Pierce Alworth <pbalworth@gmail.com>
2020-04-18 18:51:42 -0500

    Update controller to represent the serializer I want to render

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    location =  params[:location]
-    service = GoogleService.new
-    location_lat_lng = service.get_lat_lng(location)
-    
+      #all this needs to go into the forcast poro
+    location_service = GoogleService.new
+    location_data = location_service.location_info( params[:location])
+    forecast = Forecast.new
+    forecast.by_location(location_data[:geometry][:location])
+
+    render json: ForecastSerializer.new(forecast)
   end
 
 end
