f7c7d73fa3997b4756d13af676f9b8853b0d1b19
Pierce Alworth <pbalworth@gmail.com>
2020-04-18 13:41:39 -0500

    Add coverage to gitignore

---
+++ spec/requests/api/v1/forcast_from_location_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+#
+# describe "Forcast API" do
+#   it "sends weather data specific to denver" do
+#
+#     get '/api/v1/locations'
+#
+#     expect(response).to be_successful
+#   end
+# end
