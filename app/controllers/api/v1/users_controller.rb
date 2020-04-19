c58bd4ecb93e8ee0cc0a95c4443c7b3058c8cbc4
Pierce Alworth <pbalworth@gmail.com>
2020-04-19 16:47:41 -0500

    Add user controller with create functionality

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user), status: 201
+    else
+      user_message = FailedUser.new(user)
+      render json: FailedSerializer.new(user_message), status: 401
+    end
+ end
+
+  def user_params
+   params.permit(:email, :password, :password_confirmation)
+ end
+end
