7806d77fd1d0b9d5a45e1517ba496fc7f619a158
Pierce Alworth <pbalworth@gmail.com>
2020-04-18 20:26:18 -0500

    Update varibles for readability

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,11 +2,10 @@ class Api::V1::ForecastController < ApplicationController
   def show
       #all this needs to go into the forcast poro
     location_service = GoogleService.new
-    location_data = location_service.location_info( params[:location])
+    location_data = location_service.location_info(params[:location])
     forecast = Forecast.new
-    forecast.by_location(location_data[:geometry][:location])
-
-    render json: ForecastSerializer.new(forecast)
+    forecast_data = forecast.by_location(location_data) #weather object
+    render json: ForecastSerializer.new(forecast_data)
   end
 
 end
