e8c95fe54463398dbb8bc73e0c32680efa37bd46
Pierce Alworth <pbalworth@gmail.com>
2020-04-19 16:47:19 -0500

    Change password vaildation for password digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
-    it { should validate_presence_of :password }
-    it { should validate_presence_of :password_confirmation }
+    it { should validate_presence_of :password_digest }
     it { should validate_uniqueness_of :api_key }
   end
 end
