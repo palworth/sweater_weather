f55c908311b9469e3ba8fc25c25128b12c4c01f2
Pierce Alworth <pbalworth@gmail.com>
2020-04-19 16:42:22 -0500

    Add spec for user login

---
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'User registration endpoint', :type => :request do
+  it 'It can take in registration from user and give user API key' do
+
+    post '/api/v1/users', params: {
+                                    email: 'whatever@example.com',
+                                    password: 'password',
+                                    password_confirmation: 'password'}.to_json,
+                         headers: {
+                                  'Content-Type' => 'application/json',
+                                  'Accept' => 'application/json'
+                                  }
+
+    expect(response).to be_successful
+    user_key = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+    user = User.last
+    expect(user_key).to eq({api_key: user.api_key})
+  end
+
+  it 'Bad registration attempts not sucessfull and render faileduser serializer' do
+
+    post '/api/v1/users', params: {
+                                    email: 'whatever@example.com',
+                                    password: 'password',
+                                    password_confirmation: 'notthesame'}.to_json,
+                         headers: {
+                                  'Content-Type' => 'application/json',
+                                  'Accept' => 'application/json'
+                                  }
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data][:attributes].values.join).to eq("doesn't match Password")
+  end
+
+end
