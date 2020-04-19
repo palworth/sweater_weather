3dbe28324dca32364a770e6ccb4b96856a2a9abd
Pierce Alworth <pbalworth@gmail.com>
2020-04-18 22:47:40 -0500

    Create spec for background API call

---
+++ spec/requests/api/v1/background_from_location_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Background API" do
+  it "sends a picture for background specific to denver" do
+
+    location = "denver"
+
+    get "/api/v1/background?location=#{location}"
+
+    expect(response).to be_successful
+
+  end
+end
