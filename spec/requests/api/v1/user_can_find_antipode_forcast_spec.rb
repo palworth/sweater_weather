b8b5604a48d61b4e97a2e76886bdc9cbeff99208
Pierce Alworth <pbalworth@gmail.com>
2020-04-20 12:47:25 -0500

    Build out test for edgecase testing

--- spec/requests/api/v1/user_can_find_antipode_forcast_spec.rb
+++ spec/requests/api/v1/user_can_find_antipode_forcast_spec.rb
@@ -8,6 +8,11 @@ describe "Forcast API" do
     get "/api/v1/antipode?location=#{location}"
 
     expect(response).to be_successful
+    resp = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
 
+    expect(resp[:forecast][0].keys).to eq([:id, :main, :description, :icon])
+    expect(resp[:temperature]).to be_a Float
+    expect(resp[:location_name]).to be_a String
+    expect(resp[:search_location]).to be_a String
   end
 end
