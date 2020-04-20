0ecf07fca55924a81357914bc36e3337cb638a9c
Pierce Alworth <pbalworth@gmail.com>
2020-04-20 09:54:18 -0500

    fix spec for google geocode, need to merge google service & geocode

--- spec/services/google_geo_service_spec.rb
+++ spec/services/google_geo_service_spec.rb
@@ -12,15 +12,9 @@ RSpec.describe GoogleGeoApiService do
         expect(location_info[:address_components].first).to have_key(:long_name)
         expect(location_info[:address_components].first[:long_name]).to eq("Denver")
 
-        expect(location_info[:address_components][2]).to have_key(:short_name)
-        expect(location_info[:address_components][2][:short_name]).to eq("CO")
-
         expect(location_info[:address_components][3]).to have_key(:long_name)
         expect(location_info[:address_components][3][:long_name]).to eq("United States")
 
-        expect(location_info[:geometry]).to have_key(:location)
-        expect(location_info[:geometry][:location]).to have_key(:lat)
-        expect(location_info[:geometry][:location]).to have_key(:lng)
         expect(location_info[:geometry][:location][:lat]).to eq(39.7392358)
         expect(location_info[:geometry][:location][:lng]).to eq(-104.990251)
       end
