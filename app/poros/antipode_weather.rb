9703654e15f097a9c276541fdab6761053d16adf
Pierce Alworth <pbalworth@gmail.com>
2020-04-20 12:24:21 -0500

    Adjust forecast attributes to match expected results

--- app/poros/antipode_weather.rb
+++ app/poros/antipode_weather.rb
@@ -2,7 +2,7 @@ class AntipodeWeather
   attr_reader :forecast, :location_name, :search_location, :id
   def initialize(weather_service_response,location_coords, search_params)
     @id = nil
-    @forecast = weather_service_response[:daily][:data]
+    @forecast = weather_service_response[:daily][0][:weather]
     @location_name = antipode_city_name(location_coords)
     @search_location = "#{search_params}"
   end
@@ -12,20 +12,5 @@ class AntipodeWeather
     service.city_name(location_coords[:lat], location_coords[:long])
   end
 
-
-#   {
-#     "data": {
-#         "id": "null",
-#         "type": "antipode",
-#         "attributes": {
-#             "location_name": "Antipode City Name",
-#             "forecast": {
-#                 "summary": "Mostly Cloudy,
-#                 "current_temperature": "72",
-#             },
-#             "search_location": "Hong Kong"
-#         }
-#     }
-# }
 
 end
