8ad16cb9ee66aa8dfba8312effb124ec76af8ee7
Pierce Alworth <pbalworth@gmail.com>
2020-04-21 12:57:35 -0500

    Format poro

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -13,9 +13,26 @@ class RoadTrip
     directions_info[:duration][:text]
   end
 
+  def arrival_forecast
+    x = forecast_info
+    {weather_description: "#{x.description}", temperature: x.temp}
+  end
+
   private
   def directions_info
    @directions_info ||= GoogleGeoApiService.new(@origin).travel_directions(@destination)[:routes][0][:legs][0]
   end
+
+  def forecast_info
+    service = OpenWeatherService.new(destination_data).weather_info
+    x = WeatherForecast.new(service)
+  end
+
+  def destination_data
+    location_service = GoogleService.new
+    location_data = location_service.location_info(@destination)
+    Location.new(location_data)
+  end
+
 
 end
