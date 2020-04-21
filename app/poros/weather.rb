bd418c042fd0041b63580420e530a436a99060f8
Pierce Alworth <pbalworth@gmail.com>
2020-04-20 20:35:58 -0500

    Change format to better mirror proper endpoint response

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -1,10 +1,11 @@
 class Weather
-  attr_reader :current, :hourly, :daily_forecast, :id
+  attr_reader :current, :hourly, :daily_forecast, :city_state, :id
   def initialize(weather_service_response)
     @id = nil
+    @info = weather_service_response
     @current = weather_service_response[:current]
-    @hourly = weather_service_response[:hourly]
-    @daily_forecast = weather_service_response[:daily]
+    @daily_forecast = organize_week(weather_service_response[:daily][0..7])
+    @hourly = organize_day(weather_service_response[:hourly][0..6])
   end
 
   def current_humidity
@@ -21,6 +22,38 @@ class Weather
 
   def current_uvi
     @current[:uvi]
+  end
+
+  def city_state
+    x = GoogleService.new.city_name(@info[:lat], @info[:lon])
+  end
+  
+  private
+  def organize_day(hourly_forecast)
+    hourly_forecast.map do |forecast|
+      {time: format_time(forecast[:dt]),
+        temperature: forecast[:temp],
+        icon: "http://openweathermap.org/img/wn/" + forecast[:weather].first[:icon] + "@2x.png"}
+    end
+  end
+
+  def organize_week(daily_forecast)
+    daily_forecast.map do |forecast|
+      {time: format_day(forecast[:dt]),
+        forecast_summary: forecast[:weather].first[:main],
+        temperature_high: forecast[:temp][:max],
+        temperature_low: forecast[:temp][:min],
+        rain_in_mm: forecast[:rain],
+        icon: "http://openweathermap.org/img/wn/" + forecast[:weather].first[:icon] + "@2x.png"}
+    end
+  end
+
+  def format_day(time)
+    Time.at(time).strftime('%A')
+  end
+
+  def format_time(time)
+    Time.at(time).strftime("%-I %p")
   end
 
 end
