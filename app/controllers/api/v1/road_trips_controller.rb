9bd9e21efe40ee34290a685f6961b5dddc1f01fd
Pierce Alworth <pbalworth@gmail.com>
2020-04-21 11:31:36 -0500

    Add controller for road trips

---
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::RoadTripsController < ApplicationController
+  def create
+    user = User.find_by(api_key: params[:api_key])
+    if !user.nil?
+      success
+    else
+      failed
+    end
+  end
+
+  private
+  def success
+    facade = RoadTripsFacade.new(params[:origin], params[:destination])
+    road_trip_info = facade.details
+    render json: RoadtripSerializer.new(road_trip_info)
+  end
+
+  def failed
+    render json: { failed: 'Unauthorized, check API credentials' }, status: 401
+  end
+end
