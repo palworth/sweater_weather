1c39e494be6df74963dae98b23efa170f2e90cbc
Pierce Alworth <pbalworth@gmail.com>
2020-04-20 20:36:18 -0500

    Add sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#show'
       get '/background', to: 'backgrounds#show'
       post '/users', to: 'users#create'
+      post '/sessions', to: 'sessions#create'
       get '/antipode', to: 'antipodes#show'
     end
   end
