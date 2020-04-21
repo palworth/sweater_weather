3c8d3b4c2e214ca8fc62ce561babb1508e85b4cd
Pierce Alworth <pbalworth@gmail.com>
2020-04-21 13:07:13 -0500

    Add sad path test to roadtrip test

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -26,4 +26,18 @@ describe 'I can create a road trip' do
     expect(road_trip[:arrival_forecast][:temperature]).to be_a Float
     expect(road_trip[:arrival_forecast][:weather_description]).to be_a String
   end
+    it 'Will fail with bad API key', vcr: {:record => :new_episodes} do
+
+    post "/api/v1/road_trip", params: {
+                                    origin: "Denver,CO",
+                                    destination: "Fort Collins,CO"}.to_json,
+                            headers: {
+                                      'Content-Type' => 'application/json',
+                                      'Accept' => 'application/json'
+                                        }
+
+    expect(response).to_not be_successful
+    road_trip = JSON.parse(response.body, symbolize_names: true)
+    expect(road_trip[:failed]).to eq('Unauthorized, check API credentials')
+    end
 end
