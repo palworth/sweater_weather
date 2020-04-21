45202fba7a935a6c6f99f83f1bab945bfd5f7a44
Pierce Alworth <pbalworth@gmail.com>
2020-04-20 20:36:09 -0500

    Update forcast serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,5 +1,5 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :current_feels_like, :current_uvi, :daily_forecast, :hourly, :current, :current_humidity
+  attributes :id, :current_feels_like, :current_uvi, :daily_forecast, :city_state, :hourly, :current, :current_humidity
              :current_visibility
 end
