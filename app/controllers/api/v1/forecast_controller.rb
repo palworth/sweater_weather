6818ab02915fb10ef96986e04600a94462ad6138
Pierce Alworth <pbalworth@gmail.com>
2020-04-20 20:34:53 -0500

    Reformat and delete comments

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-      #all this needs to go into the forcast facade
     location_service = GoogleService.new
     location_data = location_service.location_info(params[:location])
     forecast = ForecastFacade.new
